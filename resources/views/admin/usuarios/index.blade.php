efa312fb976ef364440956799288ac81e4fcb873
Emmanuel <132721030+RojasEmmanuel@users.noreply.github.com>
2025-10-21 10:57:12 -0600

    Se corrigio el problema del la pagina de usuarios

--- resources/views/admin/usuarios/index.blade.php
+++ resources/views/admin/usuarios/index.blade.php
@@ -8,7 +8,6 @@
 <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet">
 <!-- Alternativa: Usar una versión más reciente de Font Awesome -->
-<link rel="stylesheet" href="/inicioUsuarios.css">
 @endpush
 
 @section('content')
