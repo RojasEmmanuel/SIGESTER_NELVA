0420e788369d5c23820f9a03fe2a26f1813d3ef2
RojasEmmanuel <rojasemmanuel.1202@gmail.com>
2026-03-10 19:35:06 -0600

    Pequeños cambios en el formulario de reset password

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -4,7 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Nueva Contraseña | Nelva Bienes Raíces</title>
-    <link rel="icon" type="image/png" href="{{ asset('/images/favicon.ico') }}">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link rel="stylesheet" href="{{ asset('/css/login.css') }}">
